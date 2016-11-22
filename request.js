7fb735f61ee74c9fd97e5b5f6dedc691cd93e759
jiancheng01.li <406087475@qq.com>
2016-11-22 10:36:02 +0800

    push request.js

---
+++ request.js
@@ -0,0 +1,190 @@
+import $ from 'jquery'
+
+export default request
+
+let localCache = {}
+
+function deepEquals (a, b) {
+  let keys = Object.keys
+  let isArray = Array.isArray
+
+  let isObject = o => {
+    return Object.prototype.toString.call(o) === '[object Object]'
+  }
+
+  let allAreArray = Array.isArray(a) && Array.isArray(b)
+  let allAreObject = isObject(a) && isObject(b)
+
+  if (!allAreArray && !allAreObject) {
+    return Object.is(a, b)
+  }
+
+  // array
+  if (allAreArray) {
+    // sort
+    a.sort()
+    b.sort()
+
+    let aArrLen = a.length
+    if (aArrLen !== b.length) return false
+
+    while (aArrLen--) {
+      if (!deepEquals(a[aArrLen], b[aArrLen])) return false
+    }
+  }
+
+  // object
+  if (allAreObject) {
+    let aKeys = keys(a)
+    let aKeysLen = aKeys.length
+    if (aKeysLen !== keys(b).length) return false
+
+    while (aKeysLen--) {
+      let key = aKeys[aKeysLen]
+      if (!(b.hasOwnProperty(key) && deepEquals(a[key], b[key]))) return false
+    }
+  }
+
+  return true
+}
+
+function request (url, query, options) {
+  let deferred = $.Deferred()
+
+  let needCache = options.cacheData === true
+  delete options.cacheData
+
+  let cacheData = {}
+  if (needCache) {
+    cacheData = getLocalCacheByUrl(url, query)
+
+    if (cacheData.status === 'pending') {
+      cacheData.status = 'loading'
+      cacheData.deferred = deferred
+    } else {
+      return cacheStatusHandler(cacheData, deferred)
+    }
+  }
+
+  $.ajax(extendOpts(url, query, options, deferred, needCache, cacheData))
+
+  return deferred
+}
+
+function getLocalCacheByUrl (url, query) {
+  let cacheData = localCache[url]
+
+  if (cacheData && Array.isArray(cacheData)) {
+    let matchCache = cacheData.filter(cache => {
+      return deepEquals(cache.query, query)
+    })
+
+    if (matchCache.length) {
+      return matchCache[0]
+    } else {
+      return addNewCacheAndReturn(cacheData, query)
+    }
+  } else {
+    cacheData = localCache[url] = []
+    return addNewCacheAndReturn(cacheData, query)
+  }
+}
+
+function addNewCacheAndReturn (cacheData, data) {
+  let len = cacheData.length
+
+  let defaultCache = {
+    query: data,
+    status: 'pending',
+    cache: null,
+    deferred: null
+  }
+
+  cacheData.push(defaultCache)
+
+  return cacheData[len]
+}
+
+function isObject (o) {
+  return Object.prototype.toString.call(o) === '[object Object]'
+}
+
+function cacheStatusHandler (cacheData, deferred) {
+  // 请求状态 ['pending', 'loading', 'success', 'error', 'fail']
+  let cache = cacheData.cache
+  let status = cacheData.status
+
+  if (status === 'loading') {
+    return cacheData.deferred
+  }
+
+  if (status === 'success') {
+    deferred.resolveWith(deferred, [ cache ])
+    return deferred
+  }
+
+  if (status === 'error' || status === 'fail') {
+    deferred.rejectWith(deferred, [ cache ])
+    return deferred
+  }
+}
+
+function extendOpts (url, data, options, deferred, needCache, cacheData) {
+  return $.extend({
+    url,
+    data,
+    type: 'post',
+    dataType: 'json',
+    xhrFields: {
+      withCredentials: true
+    },
+    success (res, status, xhr) {
+      ajaxSuccCb(deferred, res, status, xhr, needCache, cacheData)
+    },
+    error (xhr) {
+      ajaxFailCb(deferred, url, xhr, needCache, cacheData)
+    }
+  }, options)
+}
+
+function ajaxSuccCb (deferred, res, status, xhr, needCache, cacheData) {
+  const errorMsg = '操作失败，请稍后重试。'
+
+  if (!isObject(res)) return console.log(errorMsg)
+
+  let resData = null
+  let requestStatus = null
+
+  if (res.code === 200) {
+    deferred.resolveWith(xhr, [ res.result, res ])
+
+    resData = res.result
+    requestStatus = 'success'
+  } else {
+    console.log(res.msg || errorMsg)
+
+    deferred.rejectWith(xhr, [ res ])
+
+    resData = msg
+    requestStatus = 'error'
+  }
+
+  if(needCache) {
+    cacheData.cache = resData
+    cacheData.status = requestStatus
+    cacheData.deferred = null
+  }
+}
+
+function ajaxFailCb (deferred, url, xhr, needCache, cacheData) {
+  let msg = '请求' + url + '失败: ' + xhr.responseText
+  console.error(msg)
+
+  deferred.rejectWith(xhr)
+
+  if (needCache) {
+    cacheData.cache = msg
+    cacheData.status = 'fail'
+    cacheData.deferred = null
+  }
+}
